0e92fc772e70d797818bf4540323dc9983bf2417
Brian Thiely <brian.thiely@gmail.com>
2021-08-18 20:23:44 +0200

    valeur panier recupérer, en attente du localstorage

--- src/js/produit.js
+++ src/js/produit.js
@@ -6,15 +6,15 @@
   hydrateProduct(product)
 })()
 
-// Permet de récuperer l'ID de notre produit
+// Permet de récuperer l'ID de notre produit dans l'url
 function getProductId(){
   return new URL (location.href).searchParams.get("id")
 }
 
-// Fonction qui permet de recuperer un produit
+// Fonction qui permet de recuperer le produit choisie
 function getProduct(productId) {
   return fetch(`${apiUrl}/api/teddies/${productId}`)
-  .then((httpBodyResponse) => httpBodyResponse.json())
+  .then((res) => res.json())
   .then((products) => products)
   .catch((error) => {
     alert(
@@ -26,12 +26,12 @@ function getProduct(productId) {
 // Ajoute le choix du coloris
 function colorOptions(product){
   const productColors = product.colors
-  const productColor = document.getElementById("productColor")
+  const productOption = document.getElementById("productOption")
   productColors.forEach(color => {
-      const colorOption = document.createElement("option")
-      colorOption.setAttribute("value", color)
-      colorOption.innerHTML = color
-      productColor.appendChild(colorOption)
+      const optionColor = document.createElement("option")
+      optionColor.setAttribute("value", color)
+      optionColor.innerHTML = color
+      productOption.appendChild(optionColor)
   })
 }
 
@@ -41,5 +41,40 @@ function hydrateProduct(product) {
   document.getElementById('productImage').src = product.imageUrl
   document.getElementById('productName').textContent = product.name
   document.getElementById('productPrice').textContent = `${product.price / 100}.00 €`
-  document.getElementById('productDescription').textContent = product.description
+  document.getElementById('productDescription').textContent = product.description 
+
+
+
+        // Récupération données séléctionner pour panier et envoie du panier
+
+// Récupération de l'id du formulaire
+const idForm = document.querySelector("#productOption");
+
+
+// Sélection bouton ajout au panier
+const addToCart = document.querySelector("#add-to-cart");
+
+// Ecoute bouton addToCart
+addToCart.addEventListener("click", (e) => {
+e.preventDefault();
+
+// Choix option dans une variable
+const optionChoice = idForm.value;
+
+
+
+// Récuperation valeurs panier
+let productCart = {
+  noms: product.name,
+  couleur: optionChoice,
+  prix: product.price / 100
 }
+console.log(productCart);
+
+});   
+}
+
+
+
+
+
