4a15569ac1c657d0ef7cb3814630626f249a17f4
Brian Thiely <brian.thiely@gmail.com>
2021-08-19 18:14:06 +0200

    format prettier

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 let apiUrl =
-  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
-    ? 'http://localhost:3000'
-    : 'https://orinours.herokuapp.com/'
+	location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+		? 'http://localhost:3000'
+		: 'https://orinours.herokuapp.com/';
--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,45 @@
 // Fonction qui apelle toutes mes fonctions automatiquement
-;(async () => {
-  const products = await getProducts()
-  buildPage(products)
-})()
+(async () => {
+	const products = await getProducts();
+	buildPage(products);
+})();
 
 // Fonction qui permet de recuperer le tableau des produits
- function getProducts() {
-  return fetch(`${apiUrl}/api/teddies`)
-    .then((response) => response.json())
-    .then((products) => products)
-    .catch((error) => {
-      alert(
-        "Oups... On dirait que les nounours ce sont fait la malle !"
-      )
-    })
+function getProducts() {
+	return fetch(`${apiUrl}/api/teddies`)
+		.then((response) => response.json())
+		.then((products) => products)
+		.catch((error) => {
+			alert('Oups... On dirait que les nounours ce sont fait la malle !');
+		});
 }
 
-  // Boucle pour appliquer le template sur chaque produit
+// Boucle pour appliquer le template sur chaque produit
 function buildPage(products) {
-  products.forEach((product) => {
-    displayProduct(product)
-  })
+	products.forEach((product) => {
+		displayProduct(product);
+	});
 }
 
 function displayProduct(product) {
-  // Ou afficher le template
-  const templateElt = document.getElementById('product')
+	// Ou afficher le template
+	const templateElt = document.getElementById('product');
 
-  // Cloner le template
-  const cloneElt = document.importNode(templateElt.content, true)
+	// Cloner le template
+	const cloneElt = document.importNode(templateElt.content, true);
 
-  // Info template
-  cloneElt.getElementById('productImage').src = product.imageUrl
-  cloneElt.getElementById('productName').textContent = product.name
-  cloneElt.getElementById('productPrice').textContent = `${product.price / 100}.00 €`
-  cloneElt.getElementById('productDescription').textContent = product.description
-  cloneElt.getElementById('productLink').href = `pages/produit.html?id=${product._id}`
+	// Info template
+	cloneElt.getElementById('productImage').src = product.imageUrl;
+	cloneElt.getElementById('productName').textContent = product.name;
+	cloneElt.getElementById('productPrice').textContent = `${
+		product.price / 100
+	}.00 €`;
+	cloneElt.getElementById('productDescription').textContent =
+		product.description;
+	cloneElt.getElementById(
+		'productLink'
+	).href = `pages/produit.html?id=${product._id}`;
 
-  // Afficher template
-  document.getElementById('productsList').appendChild(cloneElt)
+	// Afficher template
+	document.getElementById('productsList').appendChild(cloneElt);
 }
